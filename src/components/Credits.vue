283300c4ad0ece0f304e78e89186a7f821b2ced0
Alberto Rico <alrico88@gmail.com>
2021-02-21 23:33:49 +0100

    🔗 Added noopener to links

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -3,9 +3,9 @@
     .col
       hr.my-2
       p.mb-2 Made by
-        a.ml-1(href="http://alrico.es", target="_blank") Alberto Rico
+        a.ml-1(href="http://alrico.es", target="_blank", rel="noopener") Alberto Rico
       p Source code available on
-        a.ml-1(href="https://github.com/alrico88/base64-viewer", target="_blank")
+        a.ml-1(href="https://github.com/alrico88/base64-viewer", target="_blank", rel="noopener")
           i.fa.fa-github-square
           |  Github
 </template>
