d54813b7e0c792ff059f4792d18d6cee66ae7263
Alberto Rico <alrico88@gmail.com>
2023-11-04 23:44:43 +0100

    feat: better input performance

--- pages/encode.vue
+++ pages/encode.vue
@@ -4,10 +4,16 @@ root-layout
     .row
       .col
         form.form
-          .form-group.mb-2
-            label.form-label The Base64 for the image is:
-            textarea.form-control(v-model="truncatedText", rows="10", readonly, disabled)
-            .form-text(v-if="needsTruncate") Text too long. Showing first 500 characters.
+          fieldset.mb-2
+            label.form-label(for="image-url") The Base64 for the image is:
+            div(:style="inputStyle")
+              codemirror(
+                id="image-url"
+                v-model="url"
+                :extensions="[EditorView.lineWrapping]"
+                :style="inputStyle"
+                disabled
+              )
           .hstack.gap-2
             button.btn.btn-sm.btn-primary(
               type="button",
@@ -24,30 +30,16 @@ root-layout
 </template>
 
 <script setup lang="ts">
-import { truncate } from "lodash-es";
+import { Codemirror } from 'vue-codemirror';
+import { EditorView } from 'codemirror';
 
-useHead({
+useSeoMeta({
   title: "Encode Image to Base64",
-  meta: [
-    {
-      name: "description",
-      content: "Encode any image to Base 64 online",
-    },
-    {
-      name: "keywords",
-      content: "image,base64,png,convert,string",
-    },
-  ],
+  description: "Encode any image to Base 64 online",
+  keywords: "image,base64,png,convert,string"
 });
 
 const url = ref("");
-
-const needsTruncate = computed(() => url.value.length > 500);
-const truncatedText = computed(() =>
-  truncate(url.value, {
-    length: 500,
-  }),
-);
 
 const { copy, copied } = useClipboard();
 
@@ -56,4 +48,6 @@ const btnDisabled = computed(() => url.value === "");
 function download() {
   useDownload(new Blob([url.value]), "base64encoded.txt");
 }
+
+const inputStyle = useInputStyle();
 </script>
