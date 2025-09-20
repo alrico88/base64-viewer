a422498c7d548e616e4d77d4a235cc7d9c32d199
Alberto Rico <alrico88@gmail.com>
2025-09-20 22:55:51 +0200

    fix: dark theme and encoder fixes

--- app/components/RootLayout.vue
+++ app/components/RootLayout.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
-.bg-gray-100(class="h-[100vh]")
+.bg-gray-100(class="h-[100vh] dark:bg-slate-700")
   .flex.h-full
     .text-center.h-full(class="w-1/2 lg:w-3/5")
       slot(name="image")
     .bg-white.border-l.border-l-gray-300.h-full.overflow-y-auto.p-4(
-      class="w-1/2 lg:w-2/5"
+      class="w-1/2 lg:w-2/5 dark:bg-slate-600 dark:border-s-gray-600"
     )
       h2.text-3xl.mb-4(class="font-[800]") Base64 viewer
       nav.tabs.tabs-border.mb-4(role="tabbox")
